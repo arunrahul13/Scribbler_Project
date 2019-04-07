be776601ee58bbfe650070759228290291d7c055
Santosh <U = santosh.pillella@gmail.com>
2019-04-07 17:22:55 +0530

    Modified the view of create model.

--- Scribbler_Project/stub/js/index.js
+++ Scribbler_Project/stub/js/index.js
@@ -1,12 +1,15 @@
 var createPostModal = document.getElementById('createPostModal');
-var buttonForCreatePost = document.getElementById("buttonForCreatePost");
+var btn3 = document.getElementById("buttonForCreatePost");
 
-var closeCreatePost = document.getElementById("closeCreatePost");
-buttonForCreatePost.onclick = function(){
-    document.getElementById('createPostContent').style.display = 'block';
-    createPostModal.style.display = "block";
+var span2 = document.getElementsByClassName("close")[2];
+btn3.onclick = function(){
+    document.getElementById('createPostModal').style.display = 'block';
+    document.getElementById('signInContent').style.display = 'none';
+    document.getElementById('signUpContent').style.display = 'none';
+    createPostModal.style.display = 'block';
 };
 
-closeCreatePost.onclick = function() {
+span2.onclick = function() {
     createPostModal.style.display = "none";
 };
+
